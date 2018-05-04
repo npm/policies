37b4feee47b8b86519f5dcd190f4efdc4746ac86
Chris Dickinson <christopher.s.dickinson@gmail.com>
2018-05-04 14:01:56 -0700

    sorry for the subshell; let us see if being more verbose works out

--- bin/write.sh
+++ bin/write.sh
@@ -15,5 +15,9 @@ for path in out/*.html; do
   candidate_slug="/policies/${name}"
   slug=${candidate_slug/\/README/}
   title=${name/-/ }
-  node_modules/.bin/mkp $slug -t "npm | policies | ${title}" <( echo '<div class="flatpages"><div class="policies"><div class="dt mw7 center pv2 pv2-m pv2-ns pa4 mb4">'; cat $path; echo '</div></div></div>' )
+
+  echo '<div class="flatpages"><div class="policies"><div class="dt mw7 center pv2 pv2-m pv2-ns pa4 mb4">' >> ${path}.tmp
+  cat $path >> ${path}.tmp
+  echo '</div></div></div>' >> ${path}.tmp
+  node_modules/.bin/mkp $slug -t "npm | policies | ${title}" ${path}.tmp
 done
